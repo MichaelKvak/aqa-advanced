bb1d7f506dd38141e2fbcee4102b14365eba42fb
Michael Kvak <mkvak1987@gmail.com>
2024-10-29 20:21:32 +0200

    add chalk module

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Цей текст синій!"));
+console.log(chalk.red.bold("Цей текст червоний та жирний!"));
+console.log(chalk.green.underline("Цей текст зелений та підкреслений!"));
