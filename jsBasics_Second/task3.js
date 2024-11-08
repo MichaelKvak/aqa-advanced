8b91b25ba2d14099ecd9cfbc12af602c44be8bc3
Michael Kvak <mkvak1987@gmail.com>
2024-11-08 14:21:50 +0200

    JS Practice Second - for, while, switch/case tasks

---
+++ jsBasics_Second/task3.js
@@ -0,0 +1,7 @@
+const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
+const number = getRandomNumber();
+
+for (let i=1; i<=10; i++) {
+    let result = number*i;
+    console.log(`${number} * ${i} = ${result}`)
+}
