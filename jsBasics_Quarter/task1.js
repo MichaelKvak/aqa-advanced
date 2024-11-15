5a3e3b6348f744b3eef367eea22272c0673fa07b
Michael Kvak <mkvak1987@gmail.com>
2024-11-15 16:03:02 +0200

    JS practice Quarter - added three tasks

---
+++ jsBasics_Quarter/task1.js
@@ -0,0 +1,18 @@
+function handleNum(number, evenCall, oddCall) {
+    if (number % 2 === 0) {
+        return evenCall()
+    }
+    else
+    return oddCall()
+}
+
+const handleEven = () => {
+    console.log('number is even')
+}
+
+const handleOdd = () => {
+    console.log('number is odd')
+}
+
+handleNum(5, handleEven, handleOdd)
+handleNum(8, handleEven, handleOdd)
