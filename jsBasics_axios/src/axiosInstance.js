7ff887d9df0518b020d296a6213cbeb1039f0118
Michael Kvak <mkvak1987@gmail.com>
2024-12-10 22:23:56 +0200

    Axios interceptors logs

--- jsBasics_axios/src/axiosInstance.js
+++ jsBasics_axios/src/axiosInstance.js
@@ -8,6 +8,39 @@ const axiosInstance = axios.create({
 	},
 });
 
+axiosInstance.interceptors.request.use(
+	(config) => {
+		console.log(`[REQUEST] ${config.method?.toUpperCase()} ${config.url}`, {
+			headers: config.headers,
+			data: config.data,
+			params: config.params,
+		});
+		return config;
+	},
+	(error) => {
+		console.error('[REQUEST ERROR]', error);
+		return Promise.reject(error);
+	},
+);
+
+axiosInstance.interceptors.response.use(
+	(response) => {
+		console.log(`[RESPONSE] ${response.config.method?.toUpperCase()} ${response.config.url}`, {
+			status: response.status,
+			data: response.data,
+		});
+		return response;
+	},
+	(error) => {
+		console.error('[RESPONSE ERROR]', {
+			message: error.message,
+			response: error.response?.data,
+			status: error.response?.status,
+		});
+		return Promise.reject(error);
+	},
+);
+
 const get = (url, params) => axiosInstance.get(url, { params });
 const post = (url, data) => axiosInstance.post(url, data);
 const put = (url, data) => axiosInstance.put(url, data);
